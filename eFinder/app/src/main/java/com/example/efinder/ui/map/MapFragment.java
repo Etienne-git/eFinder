eb9d9e9eb3afcf37a63d4e9fdb5d20bc9f3a0c6e
EtienneG <etiennegotha@gmx.de>
2021-12-08 18:01:40 +0100

    add eFinder & bottom navigation bar

---
+++ eFinder/app/src/main/java/com/example/efinder/ui/map/MapFragment.java
@@ -0,0 +1,44 @@
+package com.example.efinder.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.efinder.databinding.FragmentMapBinding;
+
+public class MapFragment extends Fragment {
+
+    private MapViewModel mapViewModel;
+private FragmentMapBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        mapViewModel =
+                new ViewModelProvider(this).get(MapViewModel.class);
+
+    binding = FragmentMapBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+        final TextView textView = binding.textMap;
+        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
