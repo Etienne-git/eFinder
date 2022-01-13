73a8047a634aabe1247c7052040b087108a294bd
Lukas Raepple <lukas.raepple@fntio.com>
2022-01-13 09:34:37 +0100

    Einzelview der Ladestation fertig

--- app/src/main/java/com/example/efinder/ViewChargingStationActivity.java
+++ app/src/main/java/com/example/efinder/ViewChargingStationActivity.java
@@ -53,6 +53,8 @@ public class ViewChargingStationActivity extends AppCompatActivity implements On
         mGoogleMap.addMarker(new MarkerOptions().position(chargingStationLocation).title("Chargingstation"));
         mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(chargingStationLocation));
 
+
+
     }
 
     @Override
@@ -73,6 +75,90 @@ public class ViewChargingStationActivity extends AppCompatActivity implements On
 
 
 
+        int chargingStationID = Integer.valueOf(intent.getStringExtra("id").toString());
+        ArrayList<ChargingStation> chargingStations = StationManager.getStation_list();
+        ArrayList<String> descriptionArray = new ArrayList<>();
+        ArrayList<String> detailsArray = new ArrayList<>();
+
+        //Get all information about chargingStation
+        String city = "Ort: " + chargingStations.get(chargingStationID).getLocation();
+        String street = "Street: " + chargingStations.get(chargingStationID).getStreet();
+        String status = "Status: besetzt";
+        String power = "Strom: " + chargingStations.get(chargingStationID).getConn_power();
+        descriptionArray.add(city);
+        descriptionArray.add(street);
+        descriptionArray.add(status);
+        descriptionArray.add(power);
+
+
+        String module_type = "Modultyp: " + chargingStations.get(chargingStationID).getModule_type();
+        String connNumber = "Verbindungsanzahl: " + chargingStations.get(chargingStationID).getNumber_of_connections();
+        String installation_date = "Installationsdatum: " + chargingStations.get(chargingStationID).getInstallation_date();
+
+        String plugType1 = "Anschlusstyp 1:   ";
+        String plugType2 = "Anschlusstyp 2:   ";
+        String plugType3 = "Anschlusstyp 3:   ";
+        String plugType4 = "Anschlusstyp 4:   ";
+
+        ArrayList<PlugType> plugTypes1 = chargingStations.get(chargingStationID).getPlug_types_1();
+
+        for(int i = 0; i < plugTypes1.size(); i++){
+            plugType1 = plugType1 + plugTypes1.get(i);
+        }
+
+        ArrayList<PlugType> plugTypes2 = chargingStations.get(chargingStationID).getPlug_types_2();
+
+        for(int i = 0; i < plugTypes2.size(); i++){
+            plugType2 = plugType2 + plugTypes2.get(i);
+        }
+
+        ArrayList<PlugType> plugTypes3 = chargingStations.get(chargingStationID).getPlug_types_3();
+
+        for(int i = 0; i < plugTypes3.size(); i++){
+            plugType3 = plugType3 + plugTypes3.get(i);
+        }
+
+        ArrayList<PlugType> plugTypes4 = chargingStations.get(chargingStationID).getPlug_types_4();
+
+        for(int i = 0; i < plugTypes4.size(); i++){
+            plugType4 = plugType4 + plugTypes4.get(i);
+        }
+
+
+        double power1 = chargingStations.get(chargingStationID).getPower_1();
+        double power2 = chargingStations.get(chargingStationID).getPower_2();
+        double power3 = chargingStations.get(chargingStationID).getPower_3();
+        double power4 = chargingStations.get(chargingStationID).getPower_4();
+
+        String power1_str = "Stromstärke: " + Double.toString(power1);
+        String power2_str = "Stromstärke: " + Double.toString(power2);
+        String power3_str = "Stromstärke: " + Double.toString(power3);
+        String power4_str = "Stromstärke: " + Double.toString(power4);
+
+
+
+        detailsArray.add(plugType1);
+        detailsArray.add(plugType2);
+        detailsArray.add(plugType3);
+        detailsArray.add(plugType4);
+
+        detailsArray.add(power1_str);
+        detailsArray.add(power2_str);
+        detailsArray.add(power3_str);
+        detailsArray.add(power4_str);
+
+        detailsArray.add(module_type);
+        detailsArray.add(connNumber);
+        detailsArray.add(installation_date);
+
+
+        ArrayAdapter<String> detailsAdapter = new ArrayAdapter<String>(ViewChargingStationActivity.this, android.R.layout.simple_list_item_1, detailsArray);
+        ArrayAdapter<String> descriptionAdapter = new ArrayAdapter<String>(ViewChargingStationActivity.this, android.R.layout.simple_list_item_1, descriptionArray);
+        detailsList.setAdapter(detailsAdapter);
+        descriptionList.setAdapter(descriptionAdapter);
+
+
+
         addfavorite.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -84,16 +170,6 @@ public class ViewChargingStationActivity extends AppCompatActivity implements On
                 int chargingStationID = Integer.valueOf(intent.getStringExtra("id").toString());
 
 
-                ArrayList<String> descriptionArray = new ArrayList();
-                descriptionArray.add("Detail1");
-                descriptionArray.add("Details2");
-                descriptionArray.add("Details3");
-                descriptionArray.add("Details4");
-                descriptionArray.add("Details5");
-
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ViewChargingStationActivity.this, android.R.layout.simple_list_item_1, descriptionArray);
-                detailsList.setAdapter(arrayAdapter);
-
 
                 System.out.println("Folgende Ladestation wurde aufgerufen: ");
                 System.out.println(chargingStationID);
@@ -104,6 +180,8 @@ public class ViewChargingStationActivity extends AppCompatActivity implements On
 
                 favoriteStations.add(chargingStations.get(chargingStationID-1));
                 FavoriteManager.setStation_list(favoriteStations);
+
+
             }
         });
 
